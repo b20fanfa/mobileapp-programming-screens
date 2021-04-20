948c0d5fefa83cdc2e4a81b8c0912781612af120
Fanny Falck <b20fanfa@student.his.se>
2021-04-20 11:35:25 +0200

    Skapade en knapp, och det går att se att knappen fungerar i "Logcat",

--- app/src/main/java/com/example/screens/LoginActivity2.java
+++ app/src/main/java/com/example/screens/LoginActivity2.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        Button button = findViewById(R.id. button1_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG","Hej hej");
+            }
+        });
     }
 }
